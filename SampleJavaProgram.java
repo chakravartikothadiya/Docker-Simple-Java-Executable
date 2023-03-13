cfbab47dc15909e2f8f342caf11598a58ed5da85
Ashish Kothadiya <chakravartikothadiya@gmail.com>
2023-03-13 13:24:44 -0400

    This is a commit for sample java program run using Docker

---
+++ SampleJavaProgram.java
@@ -0,0 +1,10 @@
+public class SampleJavaProgram{
+
+ public static void main(String[] args)
+{
+	System.out.println("This is a sample program to run using Docker");
+	System.out.println("This is version 2 with an extra print statement");
+
+}
+
+}
